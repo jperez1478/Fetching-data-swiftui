8fd3bdb5d54e28cdc0b32bc8158aedbd8152d0b9
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-07 11:33:28 -0500

    - created  an opening view to force unwrap the json format

--- FetchingData/ContentView.swift
+++ FetchingData/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onAppear(perform: {
+                let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+                URLSession.shared.dataTask(with: <#T##URLRequest#>)
+            })
     }
 }
 

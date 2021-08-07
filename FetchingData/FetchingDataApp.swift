dcc9144bd6e2680238f58b3e14a0536c52f9a788
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-07 11:26:31 -0500

    Initial Commit

---
+++ FetchingData/FetchingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingDataApp.swift
+//  FetchingData
+//
+//  Created by Jessica Perez on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FetchingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
